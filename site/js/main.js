e4bee54b4d4329d17283b1ed6601a33aae7791d6
itggot-tilda-hansson <43438049+itggot-tilda-hansson@users.noreply.github.com>
2019-05-09 09:21:42 +0200

    ..

--- site/js/main.js
+++ site/js/main.js
@@ -3,3 +3,10 @@ function menu (event){
     let menu = document.querySelector(".nav")
     menu.classList.toggle("activate")
 }
+
+function about (event){
+    let menu = document.querySelector(".dropmenu")
+    menu.classList.toggle("activate_about")
+
+
+}
