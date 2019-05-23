3a7f864bc656a5ff8fcabaf7ce2f53b2a1e32c09
itggot-tilda-hansson <43438049+itggot-tilda-hansson@users.noreply.github.com>
2019-05-23 18:10:49 +0200

    ..

--- test.js
+++
@@ -1,8 +0,0 @@
-function test (event){
-
-    let test = document.querySelector('.dropdown')
-    test.classList.toggle('aktiv')
-
-    // let test = document.querySelector("ul")
-    // menu.classList.toggle("activate")
-}
